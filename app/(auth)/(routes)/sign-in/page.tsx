92eebd5509fdf9527c1fed2d4cde56a5a8cf25bf
Parth Bhattacharya <parthget@gmail.com>
2023-09-24 11:10:07 +0530

    update git ignore

---
+++ app/(auth)/(routes)/sign-in/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const SignInPage = (props: Props) => {
+  return <div>This is an unporotected page</div>;
+};
+
+export default SignInPage;
---
+++ app/(dashboard)/(routes)/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="">
+      <p>This is a protected page</p>
+    </div>
+  );
+}
