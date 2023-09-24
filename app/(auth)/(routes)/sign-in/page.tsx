b3ffb1feace965c92b4d6b35f3b4ba527ee151bb
Parth Bhattacharya <parthget@gmail.com>
2023-09-24 11:48:50 +0530

    add authentication

--- app/(auth)/(routes)/sign-in/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-type Props = {};
-
-const SignInPage = (props: Props) => {
-  return <div>This is an unporotected page</div>;
-};
-
-export default SignInPage;
---
+++ app/test/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const TestPage = (props: Props) => {
+  return <div>This should be unprotected</div>;
+};
+
+export default TestPage;
