b7f6c460e6eedd9839d5a9e0adee6269a79e98b6
Parth Bhattacharya <parthget@gmail.com>
2023-09-29 13:08:14 +0530

    add table for course layout

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,15 +1,30 @@
-import { Button } from "@/components/ui/button";
-import React from "react";
-import Link from "next/link";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-type Props = {};
+import { db } from "@/lib/db";
 
-const CoursesPage = (props: Props) => {
+import { DataTable } from "./_components/data-table";
+import { columns } from "./_components/columns";
+
+const CoursesPage = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const courses = await db.course.findMany({
+    where: {
+      userId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
   return (
     <div className="p-6">
-      <Link href="/teacher/create">
-        <Button>New Course</Button>
-      </Link>
+      <DataTable columns={columns} data={courses} />
     </div>
   );
 };
