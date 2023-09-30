27535c1796e21811e12c4cab565a1fdbbe62cf9a
Parth Bhattacharya <parthget@gmail.com>
2023-09-30 08:19:45 +0530

    setup stripe

--- app/(course)/courses/[courseId]/_components/course-sidebar-item.tsx
+++ app/(course)/courses/[courseId]/_components/course-sidebar-item.tsx
@@ -54,7 +54,7 @@ export const CourseSidebarItem = ({
       </div>
       <div
         className={cn(
-          "ml-auto opacity-0 border-2 border-slate-700 h-full transition-all",
+          "ml-auto opacity-0 border-2 border-lime-700 h-full transition-all",
           isActive && "opacity-100",
           isCompleted && "border-emerald-700"
         )}
